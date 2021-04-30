907ecbc1fd414f18845976abade2b8af546b5c7b
Thomas Maxey <tmaxey201@gmail.com>
2021-04-30 14:23:47 -0400

    created function for an onclick even that saves input to local storage and adds it back when page reloads

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
+var nowHour = parseInt(moment().format("H"));
+var saveButton = $('.row').children('.btn');
 
 
 // added current day into header 
 $("#today").text(moment().format("dddd, MMMM YYYY"))
 
 
+
+// create function with an on click button event that saves content within text box to local storage and then inputs saved content to text box when page is reloaded.
+saveButton.click(function(event) {
+    event.preventDefault();
+    var inputTxt = $(this.parentNode).children('.content').attr('id');
+    var inputToStorage = $(this.parentNode).children('.content').val();
+    localStorage.setItem(inputTxt, inputToStorage);
+})
