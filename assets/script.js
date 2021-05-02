5ccb04738f4df6c97c90b4ab283b7a5442481025
Thomas Maxey <tmaxey201@gmail.com>
2021-05-01 22:21:16 -0400

    created an array.from and parseInt to turn the row into an integer to use in a future function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var nowHour = parseInt(moment().format("H"));
 var saveButton = $('.row').children('.btn');
-
+var rows = document.getElementsByClassName('row')
 
 // added current day into header 
 $("#today").text(moment().format("dddd, MMMM YYYY"))
@@ -20,3 +20,37 @@ for (var index = 1; index < 10; index++) {
     $(`#text${index}`).text(localStorage.getItem(`text${index}`));
     
 }
+
+// create a method of changing the color of the text box depending on the time(nowHour)
+
+
+
+Array.from(rows).forEach(row => {           //used Array.from to create an array from all of the "row" classes
+    var rowIDString = row.id,
+    rowHour;                               
+if (rowIDString) {
+    rowHour = parseInt(rowIDString);        //used parseInt to parse a string and return it as an integer. 
+}
+if (rowHour) {
+    if (nowHour === rowHour) {              //used an if else if to change the class of the row to reflect the time. 
+        setClasspresent(row);
+    } else if ((nowHour < rowHour) && (nowHour >= rowHour - 8)) {
+        setClassfuture(row);
+    } else if ((nowHour < rowHour) && (nowHour <= rowHour + 8)) {
+        setClasspast(row);
+    } else {
+        setClasspast(row);
+    }
+}
+});
+
+
+function setClasspast(element, color) {        //created a function to change the row class to "past","present", or "future". 
+    element.classList.add("past");
+  }
+  function setClassfuture(element, color) {
+    element.classList.add("future");
+  }
+  function setClasspresent(element, color) {
+    element.classList.add("present");
+  }
