a32892853200fca49906e9f13aae550e7c79fa33
Thomas Maxey <tmaxey201@gmail.com>
2021-05-01 22:22:37 -0400

    created an array.from to turn the list of rows into an array and then used parseInt to parse it into an integer

--- assets/script.js
+++ assets/script.js
@@ -31,26 +31,3 @@ Array.from(rows).forEach(row => {           //used Array.from to create an array
 if (rowIDString) {
     rowHour = parseInt(rowIDString);        //used parseInt to parse a string and return it as an integer. 
 }
-if (rowHour) {
-    if (nowHour === rowHour) {              //used an if else if to change the class of the row to reflect the time. 
-        setClasspresent(row);
-    } else if ((nowHour < rowHour) && (nowHour >= rowHour - 8)) {
-        setClassfuture(row);
-    } else if ((nowHour < rowHour) && (nowHour <= rowHour + 8)) {
-        setClasspast(row);
-    } else {
-        setClasspast(row);
-    }
-}
-});
-
-
-function setClasspast(element, color) {        //created a function to change the row class to "past","present", or "future". 
-    element.classList.add("past");
-  }
-  function setClassfuture(element, color) {
-    element.classList.add("future");
-  }
-  function setClasspresent(element, color) {
-    element.classList.add("present");
-  }
